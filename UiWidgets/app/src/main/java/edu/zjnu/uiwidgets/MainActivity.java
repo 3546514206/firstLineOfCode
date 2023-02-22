a7cb6a1d54e97b7d5f0534f3c22ffafda979edd1
杨海波 <3546514206@qq.com>
2023-02-22 14:40:38 +0800

    图片显示

--- UiWidgets/app/src/main/java/edu/zjnu/uiwidgets/MainActivity.java
+++ UiWidgets/app/src/main/java/edu/zjnu/uiwidgets/MainActivity.java
@@ -1,14 +1,44 @@
 package edu.zjnu.uiwidgets;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Map<String, View> viewMap = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+        EditText text = findViewById(R.id.text);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        viewMap.put("button", button);
+        viewMap.put("text", text);
+        viewMap.put("imageView", imageView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText text = (EditText) viewMap.get("text");
+        String toastContent = null;
+        if (text != null) {
+            toastContent = text.getText().toString();
+        }
+        Toast.makeText(this, toastContent, Toast.LENGTH_LONG).show();
     }
 }
