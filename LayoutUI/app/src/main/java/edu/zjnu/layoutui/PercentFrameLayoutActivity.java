0cb491376313c8233e906e7e20414d7807da1047
杨海波 <3546514206@qq.com>
2023-03-03 22:17:38 +0800

    百分比布局（bushi）

---
+++ LayoutUI/app/src/main/java/edu/zjnu/layoutui/PercentFrameLayoutActivity.java
@@ -0,0 +1,14 @@
+package edu.zjnu.layoutui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PercentFrameLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_frame_layout);
+    }
+}
