c6358d096bc9d52089cbbf7105775eba2ba36529
杨海波 <3546514206@qq.com>
2023-03-03 23:40:48 +0800

    最常用的控件Listview

---
+++ ListViewSample/app/src/main/java/edu/zjnu/listviewsample/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.zjnu.listviewsample;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] data = {"1111", "2222", "3333", "4444", "5555", "6666", "7777", "8888", "9999"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> array = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        ListView view = findViewById(R.id.listView);
+        view.setAdapter(array);
+    }
+
+}
